0abbfb79485f9bcd85cfe6c65a87137faa1786cc
Easyseob <83761245+Soldbone@users.noreply.github.com>
2025-04-29 21:49:16 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 380 ms, Memory: 58616 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,25 +1,43 @@
-n = int(input())
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline().rstrip())
 
 graph = [ [] for _ in range(n+1)]
 for _ in range(n-1):
-    x, y = list(map(int, input().split()))
+    x, y = list(map(int, sys.stdin.readline().split()))
     graph[x].append(y)
     graph[y].append(x)
 
 visited = [False] * (n+1)
 result = {}
-def dfs(graph, v, visited):
-    stack = [v]
-    visited[v] = True
-    while stack:
-        value = stack.pop()
-        if not visited[value]:
-            visited[value] = True
-        for j in graph[value]:
-            if not visited[j]:
-                result[j] = value
-                stack.append(j)
+# def dfs(graph, v, visited):
+#     stack = [v]
+#     visited[v] = True
+#     while stack:
+#         value = stack.pop()
+#         if not visited[value]:
+#             visited[value] = True
+#         for j in graph[value]:
+#             if not visited[j]:
+#                 result[j] = value
+#                 stack.append(j)
 
-dfs(graph, 1, visited)
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    
+    while queue:
+        v = queue.popleft()
+        # print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                result[i] = v
+                queue.append(i)
+                visited[i] = True
+
+
+# dfs(graph, 1, visited)
+bfs(graph, 1, visited)
 for i in range(2, n+1):
     print(result[i])
