cd92ba417df1a1a336226636380633a5be15dd0e
Easyseob <83761245+Soldbone@users.noreply.github.com>
2025-07-15 18:10:13 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,30 +1,32 @@
 # 유기농 배추
 # 이번에도 풀이를 보긴 했지만 내 아이디어로 구현했다.
+# [Improve] 배추를 담든 자료형을 set으로 변경해봤다. remove -> discard : O(N) -> O(1)
 
 import sys
 from collections import deque
 input = sys.stdin.readline
 
 # 1697(숨바꼭질)에서 사용했던 bfs 코드 참고함
-def bfs(napa_cabbage_coord: list):
+def bfs(napa_cabbage_coord: set):
     # Respectively, up/right/down/left
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    # dx = [0, 1, 0, -1]
+    # dy = [1, 0, -1, 0]
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-    q = deque([napa_cabbage_coord[0]])
-    del napa_cabbage_coord[0]
+    q = deque([napa_cabbage_coord.pop()])
     while q:
-        v = q.popleft()
+        x, y = q.popleft()
     
-        for i in range(4):
-            if (v[0] + dx[i], v[1] + dy[i]) in napa_cabbage_coord:
-                q.append((v[0] + dx[i], v[1] + dy[i]))
-                napa_cabbage_coord.remove((v[0] + dx[i], v[1] + dy[i]))
+        for dx, dy in directions:
+            nx, ny = x+dx, y+dy
+            if (nx, ny) in napa_cabbage_coord:
+                q.append((nx, ny))
+                napa_cabbage_coord.discard((nx, ny))
 
 T = int(input())
 for _ in range(T):
     M, N, K = map(int, input().split())
-    napa_cabbage_coord = [ tuple(map(int, input().split())) for _ in range(K) ]
+    napa_cabbage_coord = { tuple(map(int, input().split())) for _ in range(K) }
 
     count = 0
     for i in range(K):
