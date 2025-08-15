41c29c4ac2fac772b285a8068f5313195c5318a9
Easyseob <83761245+Soldbone@users.noreply.github.com>
2025-08-15 15:35:17 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,11 @@
-# 설탕 배달
-
-# [16] → 5*2 + 3*2
-# [11] → 3: 5*1 + 3*2
-
 N = int(input())
 
 result = -1
 num_of_5kg = N//5
 for i in range(num_of_5kg, -1, -1):
     remain = (N - 5 * i)
-    if ( remain % 3 == 0 ):
-        result = i + remain // 3
+    num_of_3kg = remain // 3
+    if ( remain - 3 * num_of_3kg == 0 ):
+        result = i + num_of_3kg
         break
 print(result)
