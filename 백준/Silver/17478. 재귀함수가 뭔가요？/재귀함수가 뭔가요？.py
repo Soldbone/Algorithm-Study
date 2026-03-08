034990c69258b8e86811d536d563d693afa72875
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-08 23:03:28 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -1,16 +1,33 @@
-# 재귀함수가 뭔가요?
-n = int(input())
+# 재귀함수 - 재귀함수가 뭔가요? (백준 실버5)
+# 문제 링크: https://www.acmicpc.net/problem/17478
 
-def what_is_recursion(count):
-    print(count*'____' + '"재귀함수가 뭔가요?"')
-    if (count < n):
-        print(count*'____' + '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
-        print(count*'____' + '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
-        print(count*'____' + '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
-        what_is_recursion(count+1)
-    else:
-        print(count*'____' + '"재귀함수는 자기 자신을 호출하는 함수라네"')
-    print(count*'____' + '라고 답변하였지.')
 
+def recursive(n: int) -> None:
+    prefix = 4 * (N - n) * "_"
+
+    print(prefix + '"재귀함수가 뭔가요?"')
+
+    if n == 0:
+        print(prefix + '"재귀함수는 자기 자신을 호출하는 함수라네"')
+        print(prefix + "라고 답변하였지.")
+        return
+
+    print(
+        prefix
+        + '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
+    )
+    print(
+        prefix
+        + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
+    )
+    print(
+        prefix
+        + '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'
+    )
+    recursive(n - 1)
+    print(prefix + "라고 답변하였지.")
+
+
+N = int(input())
 print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-what_is_recursion(0)
+recursive(N)
