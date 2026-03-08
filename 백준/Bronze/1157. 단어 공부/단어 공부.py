97eff5a757809519300f8d2e90518a117e1df234
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-08 22:37:12 +0900

    [Bronze I] Title: 단어 공부, Time: 264 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,28 +1,16 @@
-# 문자열 - 단어 공부 (백준 브론즈1)
-# 문제 링크: https://www.acmicpc.net/problem/1157
+"""
+ChatGPT의 도움을 받아 개선한 버전
+"""
+word = input().upper()
 
-word = input()
-count_for_char = {}
-max_count = 1
-# 등록 시 최대값을 기억하도록 약간 개선
+counts_for_alpha = [0] * 26
+
 for char in word:
-    ignore_case_char = char.upper()
-    if ignore_case_char not in count_for_char:
-        count_for_char[ignore_case_char] = 1
-    else:
-        count_for_char[ignore_case_char] += 1
-        count = count_for_char[ignore_case_char]
-        if count > max_count:
-            max_count = count
+    counts_for_alpha[ord(char) - ord("A")] += 1
 
-# 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력
-# 순회해서 가장 큰 값을 갖는 키를 리스트에 저장하고 리스트의 길이를 확인한다.
-most_freq_chars = []
-for char, count in count_for_char.items():
-    if count == max_count:
-        most_freq_chars.append(char)
+max_count = max(counts_for_alpha)
 
-if len(most_freq_chars) == 1:
-    print(most_freq_chars[0])
+if counts_for_alpha.count(max_count) == 1:
+    print(chr(counts_for_alpha.index(max_count) + ord("A")))
 else:
     print("?")
