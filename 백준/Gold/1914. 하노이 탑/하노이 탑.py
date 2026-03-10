16545d86c2c90b921b937c0a906fcae32879ebd6
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-11 02:11:45 +0900

    [Gold V] Title: 하노이 탑, Time: 696 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,15 +1,22 @@
-# 하노이탑 (복습 필요)
+# 재귀함수 - 하노이 탑 (백준 골드5)
+# 문제 링크: https://www.acmicpc.net/problem/1914
+N = int(input())
 
-# A -> C via B
-def hanoi(n, a, c, b):
-    if (n == 1):
-        print(a, c)
+# 하노이 옮기는 개수 점화식.
+# n번째 블록 위의 T(n-1)개를 옮기고  n번째 블록을 제일 오른쪽으로 옮기고 n
+count = 2**N - 1
+print(count)
+
+def hanoi(src, dest, via, n):
+    if n > 20:
+        return
+
+    if n == 1:
+        print(src, dest)
+        return
     else:
-        hanoi(n-1, a, b, c)
-        print(a, c)
-        hanoi(n-1, b, c, a)
+        hanoi(src, via, dest, n-1)
+        print(src, dest)
+        hanoi(via, dest, src, n-1)
 
-n = int(input())
-print(2**n-1)
-if (n <= 20):
-    hanoi(n, 1, 3, 2)
+hanoi("1", "3", "2", N)
