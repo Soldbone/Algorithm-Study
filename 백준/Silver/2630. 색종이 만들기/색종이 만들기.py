761143ea80038ffca691b33717bf66c5bd4832f8
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-16 01:53:06 +0900

    [Silver II] Title: 색종이 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,38 +1,50 @@
-# 색종이 만들기
-n = int(input())
+import sys
 
-paper_lst = []
-for _ in range(n):
-    paper_lst.append(list(map(int, input().split())))
+input = sys.stdin.readline
+N = int(input())
 
-white, blue = 0, 0
+entire_paper = [list(map(int, input().split())) for _ in range(N)]
 
-def check_and_count_paper(n, i, j):
-    global white
-    global blue
-    IS_WHITE = True
-    IS_BLUE = True
-    for row in range(i, i+n):
-        if paper_lst[row][j:j+n] != [0] * n:
-            IS_WHITE = False
-        if paper_lst[row][j:j+n] != [1] * n:
-            IS_BLUE = False
 
-    if IS_WHITE:
-        white += 1
-        return True
-    if IS_BLUE:
-        blue += 1
-        return True
-    
-    return False
+blue_count = 0
+white_count = 0
 
-def rec_divide_paper(n, i, j):
-    if check_and_count_paper(n, i, j):
-        return True
+
+def make_paper(row_start, col_start, n):
+    # end 구간은 반열림 구간으로 둬서 숫자가 1큼에 유의
+    global blue_count
+    global white_count
+
+    row_end = row_start + n
+    col_end = col_start + n
+    color = entire_paper[row_start][col_start]
+    if row_end - row_start == 1:
+        if color == 0:
+            white_count += 1
+        else:
+            blue_count += 1
+        return
+
+    for row in range(row_start, row_end):
+        for col in range(col_start, col_end):
+            if color != entire_paper[row][col]:
+                # 왼쪽 위:      [0 ~ N//2][0 ~ N//2]
+                make_paper(row_start, col_start, n // 2)
+                # 오른쪽 위:    [0 ~ N//2][N//2 ~ N]
+                make_paper(row_start, col_start + n // 2, n // 2)
+                # 왼쪽 아래:    [N//2 ~ N][0 ~ N//2]
+                make_paper(row_start + n // 2, col_start, n // 2)
+                # 오른쪽 아래:  [N//2 ~ N][N//2 ~ N]
+                make_paper(row_start + n // 2, col_start + n // 2, n // 2)
+                return
+
+    # 색이 모두 같으면 count하고 return
+    if color == 0:
+        white_count += 1
     else:
-        n //= 2
-        return rec_divide_paper(n, i, j), rec_divide_paper(n, i+n, j), rec_divide_paper(n, i, n+j), rec_divide_paper(n, i+n, n+j)
-    
-rec_divide_paper(n, 0, 0)
-print(white, blue, sep='\n')
+        blue_count += 1
+    return
+
+
+make_paper(0, 0, N)
+print(white_count, blue_count, sep="\n")
