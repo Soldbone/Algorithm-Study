8a0938f20a8560bde8e9f71f311a9bb1a26e7f80
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-16 15:19:13 +0900

    [Silver II] Title: 에디터, Time: 228 ms, Memory: 38732 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,21 +1,23 @@
 import sys
-
 input = sys.stdin.readline
+
 left = list(input().rstrip())
 right = []
-M = int(input())
-for _ in range(M):
-    command = input().split()
-    match command[0]:
-        case "L":
-            if left:
-                right.append(left.pop())
-        case "D":
-            if right:
-                left.append(right.pop())
-        case "B":
-            if left:
-                left.pop()
-        case "P":
-            left.append(command[1])
-print("".join(left + right[::-1]))
+
+for _ in range(int(input())):
+    cmd = input().rstrip()
+
+    if cmd[0] == 'L':
+        if left:
+            right.append(left.pop())
+    elif cmd[0] == 'D':
+        if right:
+            left.append(right.pop())
+    elif cmd[0] == 'B':
+        if left:
+            left.pop()
+    else:  # P x
+        left.append(cmd[2])
+
+left.extend(reversed(right))
+print(''.join(left))
