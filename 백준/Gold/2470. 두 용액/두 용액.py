addd9c0831329841bf24e4150153e0998d2eb8a8
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-17 16:39:00 +0900

    [Gold V] Title: 두 용액, Time: 104 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+liquid = sorted(list(map(int, input().split())))
+
+i = 0
+j = len(liquid) - 1 - i
+min = float("inf")
+min_index = (None, None)
+while i < j:
+    mixture = liquid[i] + liquid[j]
+    if abs(mixture) < abs(min):
+        min = mixture
+        min_index = (i, j)
+
+    if mixture >= 0:
+        j -= 1
+    else:
+        i += 1
+
+print(liquid[min_index[0]], liquid[min_index[1]])
