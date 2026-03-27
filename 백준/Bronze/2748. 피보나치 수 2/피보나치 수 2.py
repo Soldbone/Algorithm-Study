82cd6b16a7a89f21958fd321334417376afff76a
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-28 00:11:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,19 +1,7 @@
-# DP - 피보나치 수 2 (백준 브론즈 1)
-# 문제 링크: https://www.acmicpc.net/problem/2748
-
-def fibo(n, memo=None):
-    if memo is None:
-        memo = dict()
-    
-    if n <= 1:
-        return n
-    
-    if n in memo:
-        return memo[n]
-    
-    memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
-    return memo[n]
-
-
-n = int(input())
-print(fibo(n))
+import sys
+n = int(sys.stdin.readline())
+dp = [0] * (n + 1)
+dp[1] = 1
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+print(dp[n])
