98af01b2100c6138dd7b09861f2c617e52540293
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-31 23:13:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 220 ms, Memory: 111544 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -5,5 +5,5 @@ dp = [0] * (K + 1)
 for i in range(len(items)):
     for w in range(K, 0, -1):
         if w >= items[i][0]:
-            dp[w] = max(dp[w], dp[w - 1], dp[w - items[i][0]] + items[i][1])
+            dp[w] = max(dp[w], dp[w - items[i][0]] + items[i][1])
 print(dp[K])
