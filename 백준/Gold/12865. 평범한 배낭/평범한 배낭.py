37f795c7b03e2f797c8a35311aa13b1aeea6612c
이지섭 <83761245+Soldbone@users.noreply.github.com>
2026-03-31 23:25:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 110328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,9 +1,10 @@
+# DP - 평범한 배낭 (백준 골드5)
+# 문제 링크: https://www.acmicpc.net/problem/12865
 N, K = map(int, input().split())
 items = [tuple(map(int, input().split())) for _ in range(N)]
 dp = [0] * (K + 1)
 
-for i in range(len(items)):
-    for w in range(K, 0, -1):
-        if w >= items[i][0]:
-            dp[w] = max(dp[w], dp[w - items[i][0]] + items[i][1])
+for weight, value in items:
+    for w in range(K, weight - 1, -1):
+        dp[w] = max(dp[w], dp[w - weight] + value)
 print(dp[K])
